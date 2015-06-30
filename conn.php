d7c85c6492d7a0ba3f8707844cfe359e0158e2c2
waltim <waltimlmm@gmail.com>
2015-06-30 09:15:24 -0300

    setimo commit

--- conn.php
+++ conn.php
@@ -2,11 +2,11 @@
 //$conn = mysql_connect('127.0.0.1','root','') or die(mysql_error());
 //$banco = mysql_select_db('conta') or die(mysql_error());
 
-$conn = new MongoClient();
-
-$db = $conn->conta;
-
-$colecao = $db->conta;
+//$conn = new MongoClient();
+//
+//$db = $conn->conta;
+//
+//$colecao = $db->conta;
 
 
 ?>
